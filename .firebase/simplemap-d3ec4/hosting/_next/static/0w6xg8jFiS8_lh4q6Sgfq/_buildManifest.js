c5da882a1e5020aae77ab9946cc7c5af71bee8af
Ron <ronnieyahav@gmail.com>
2024-06-03 20:04:23 +0300

    getDemagogues api endpoint
    api endpoint works /api/getDemagogues/?twitterHandle="PARAM"

--- .firebase/simplemap-d3ec4/hosting/_next/static/0w6xg8jFiS8_lh4q6Sgfq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/a6890a3306711e32.css","static/chunks/pages/index-860affdba981923f.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
