33489c061543c8c508418174d009c3afad99b469
Ilay Kavitzky <ilaykavitzky@gmail.com>
2024-06-03 19:06:48 +0300

    Revert "Added demagogues_view page"
    This reverts commit b2d75ced6432b71cb2d1bf7c86a38855c2b99559.

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,9 @@
-import ReactDOM from "react-dom/client";
-// import { Link } from 'react-router';
-import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
-import Blogs from "./demagogues_view";
-import Link from 'next/link'
-
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { useNavigate } from "react-router-dom";
-
 
 export default function Home() {
-  // let navigate = useNavigate();
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    // Placeholder
-    serverResponse = JSON.stringify({
-      "demagogues":
-        [
-          { "handle": "@bot1", "hate_rate": 10, "fake_rate": 73 },
-          { "handle": "@bot2", "hate_rate": 20, "fake_rate": 33 },
-          { "handle": "@bot3", "hate_rate": 30, "fake_rate": 43 }
-        ]
-    })
-
-    demagogues = JSON.parse(serverResponse)
-    // navigate(path)
     // Add your form submission logic here
   };
 
@@ -35,11 +13,11 @@ export default function Home() {
     method: 'GET',
     mode: 'no-cors'
   })
-    .then(response => response.text())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
+  .then(response => response.text())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
 
-
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -54,12 +32,9 @@ export default function Home() {
           <input type="text" placeholder="Enter your text" />
           <button type="submit">Submit</button>
         </form>
-        <Link legacyBehavior href={{ pathname: 'get_demagogues', query: { data: JSON.stringify({ 'a': 1 }) } }}>
-          <a>About us</a>
-        </Link>
       </main>
 
       {/*... */}
-    </div >
+    </div>
   );
 }
